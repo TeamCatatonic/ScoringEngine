695c10d390b6ad6d4e6340312c92d7b31f1c2a21
Aaron Opell <aaron.opell@live.com>
2017-05-14 16:41:48 -0700

    Fancier HTML file

--- ScoringEngine/ScoringEngine/ScoredItemParser.cs
+++ ScoringEngine/ScoringEngine/ScoredItemParser.cs
@@ -49,15 +49,20 @@ namespace ScoringEngine
                 if (!item.IsPenalty) maxScore += item.Points;
             }
             string templated = File.ReadAllText("ScoringTemplate.html");
-            Dictionary<string, string> toReplace = new Dictionary<string, string>()
+            KeyValueList<string, string> toReplace = new KeyValueList<string, string>
             {
                 { "TIME_STRING", DateTime.Now.ToString() },
                 { "SCORE_STRING", $"{totalScore}/{maxScore}" },
+                { "RANK_STRING", "1st place" },
+                { "TEAM_COUNT_STRING", "of 12 teams" },
+                { "COLOR_STRING", scoredPenalties.Count > 0 ? "red" : "blue" },
+                { "PEN_SECTION", scoredPenalties.Count > 0 ? "<div class='col s12'><div class='card-panel red lighten-1 white-text'><h3 class='header'>Penalties</h3><h5>{{ PEN_STRING }}</h5><ul class='collection red'>{{ PEN_LIST }}</ul></div></div>" : "" },
                 { "VULN_STRING", $"{scoredVulns.Count}/{items.Where(x => !x.IsPenalty).Count()} ({scoredVulns.Sum(x => x.Points)} pts)" },
-                { "VULN_LIST", String.Join(Environment.NewLine, scoredVulns.Select(item=> $"<li class='collection-item'>{item.Description} - {item.Points} pts</li>")) },
-                { "PEN_STRING", $"{scoredPenalties.Count} ({scoredPenalties.Sum(x => x.Points)} pts)" },
-                { "PEN_LIST", String.Join(Environment.NewLine, scoredPenalties.Select(item=>$"<li class='collection-item'>{item.Description} - {item.Points} pts</li>"))},
+                { "VULN_LIST", String.Join(Environment.NewLine, scoredVulns.Select(item=> $"<li class='collection-item blue-grey'>{item.Description} - {item.Points} pts</li>")) },
+                { "PEN_STRING", $"{scoredPenalties.Count} (-{scoredPenalties.Sum(x => x.Points)} pts)" },
+                { "PEN_LIST", String.Join(Environment.NewLine, scoredPenalties.Select(item=>$"<li class='collection-item red lighten-1'>{item.Description} - {item.Points} pts</li>"))},
             };
+
             foreach (var item in toReplace)
             {
                 Regex regex = new Regex("{{\\s+" + item.Key + "\\s+}}");
@@ -91,6 +96,14 @@ namespace ScoringEngine
                     return OS.MacOS;
                 default:
                     return OS.Other;
+            }
+        }
+
+        public class KeyValueList<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+        {
+            public void Add(TKey key, TValue value)
+            {
+                Add(new KeyValuePair<TKey, TValue>(key, value));
             }
         }
     }
